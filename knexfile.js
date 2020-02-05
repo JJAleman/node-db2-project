9e672f439dded87058ff4adb8d49d9e7fd9f12e9
Juan Aleman <juan.j.aleman@gmail.com>
2020-02-04 20:11:01 -0600

    configured knex to specification on TK

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,24 @@
-// Update with your config settings.
+// create this file with: knex init be sure you are in the root of your repo! or
+// use --knexfile ... don't do what i've done before lol (create knexfile.js in
+// the ./data folder... made all sortsa prolems.).
 
 module.exports = {
 
-  development: {
+
+  development: {  
     client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
 
-  staging: {
-    client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/car-dealer.db3'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
+
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
-
-};
+}
